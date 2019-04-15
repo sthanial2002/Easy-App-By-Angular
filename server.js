8520ee8b007ceda6cc38c09ce9a6af2c70ee779d
Maurel OUSSOU <micronet@consultant.com>
2019-04-15 15:27:35 +0100

    Edit server.js's file

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/TPAngular'));
+app.use(express.static(__dirname + '/dist/easyapp-by-angular'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/TPAngular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/easyapp-by-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
